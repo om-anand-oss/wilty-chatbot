74b2a295fbcee52eca39a591714170ac6df576a0
Om Anand <oma2668@gmail.com>
2023-10-02 19:14:24 -0300

    Parse excel and send data

---
+++ data/scripts/parseExcel.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+
+def optionalParameters():
+  return ["This is My Guest", "Possession", "Unseen bits"]
+
+
+def addToDictIfExists(df, idx, parameterName, dataDict):
+  if df[parameterName][idx]:
+    dataDict[parameterName] = df[parameterName][idx]
+  return dataDict
+
+
+def AddOptionalParameters(df, idx, dataDict, parameterList):
+  for parameterName in parameterList:
+    if df[parameterName][idx]:
+      dataDict[parameterName] = df[parameterName][idx]
+  return dataDict
+
+
+def getDataDict():
+  df = pd.read_excel("data/wiltyData.xlsx")
+
+  df = df.fillna("")
+
+  data = []
+  for idx in df.index:
+    newPrompt = {
+        "participant": df["Participant"][idx],
+        "isItTrue": df["IsItReal"][idx],
+        "series": df["Series"][idx],
+        "episode": df["Episode"][idx],
+        "roundType": df["Round Type"][idx],
+        "prompt": df["Prompt"][idx],
+        "teamCaptain": df["Team Captain"][idx],
+    }
+
+    newPrompt = AddOptionalParameters(df, idx, newPrompt, optionalParameters())
+
+    # print(newPrompt)
+    data.append(newPrompt)
+    # print(participant, isItReal, len(data[participant][isItReal]))
+  # print(len(data))
+  return data
---
+++ data/scripts/sendDataToDynamoDB.py
@@ -0,0 +1,79 @@
+import boto3
+from parseExcel import getDataDict, optionalParameters
+
+client = boto3.client("dynamodb")
+# print(response)
+
+
+def filterItemValue(itemValue):
+  # print(itemValue, type(itemValue))
+  if itemValue is None:
+    return ""
+  return str(itemValue)
+
+
+def createDynamoDBItem(prompt):
+  Item = {
+      "participant": {
+          "S": filterItemValue(prompt["participant"]),
+      },
+      "isItTrue": {
+          "S": filterItemValue(prompt["isItTrue"]),
+      },
+      "series": {
+          "N": filterItemValue(prompt["series"]),
+      },
+      "episode": {
+          "N": filterItemValue(prompt["episode"]),
+      },
+      "roundType": {
+          "S": filterItemValue(prompt["roundType"]),
+      },
+      "prompt": {
+          "S": filterItemValue(prompt["prompt"]),
+      },
+      "teamCaptain": {
+          "S": filterItemValue(prompt["teamCaptain"]),
+      },
+  }
+
+  for parameter in optionalParameters():
+    if parameter in prompt:
+      Item[parameter] = {"S": filterItemValue(prompt[parameter])}
+  return Item
+
+
+def createPutRequest(prompt):
+  putRequest = {
+      "PutRequest": {
+          "Item": createDynamoDBItem(prompt)
+      }
+  }
+  return putRequest
+
+
+def createBatchWrite(data, tableName):
+  batchWriteRequest = {
+      tableName: []
+  }
+
+  for prompt in data:
+    batchWriteRequest[tableName].append(createPutRequest(prompt=prompt))
+  return batchWriteRequest
+
+
+def sendDataToDB(data, tableName):
+  for i in range(0, len(data), 25):
+    startIndex = i
+    endIndex = min(i+25, len(data))
+
+    response = client.batch_write_item(
+        RequestItems=createBatchWrite(
+            data[startIndex:endIndex], tableName=tableName),
+        ReturnConsumedCapacity='TOTAL',
+        ReturnItemCollectionMetrics='SIZE'
+    )
+    print(response)
+
+
+sendDataToDB(getDataDict(), "wilty")
